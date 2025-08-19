21fe64965b6c61aa9e9d6ac784c30c2ec4797a29
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-08-19 12:06:43 +0530

    Day27_SimpleProductsDemo

---
+++ MVC_DEMO/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿//Adding model
+using MVC_DEMO.Models; // Uncomment if you have a Product model to use
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_DEMO.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //Controllers are responsible for handling user input and returning responses
+
+        // get: /Products/Index
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Laptop", Price = 999.00M },
+                new Product { Id = 2, Name = "Smartphone", Price = 250.00M },
+                new Product { Id = 3, Name = "Tablet", Price = 550.00M }
+
+            };
+            return View(products); // Passing the list of products to the view
+
+        }
+    }
+}
---
+++ MVC_DEMO/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_DEMO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
