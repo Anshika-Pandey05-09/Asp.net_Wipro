3ebd9262b70b6aa79dfa14562e0a6cd750898a8f
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-08-19 20:19:09 +0530

    Day27_BookCatalogDemo_Using_CRUD_Operation

---
+++ BookCatalog/Models/Book.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Published Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime PublishedDate { get; set; }
+    }
+}
