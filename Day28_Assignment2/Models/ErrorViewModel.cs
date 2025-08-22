8f16c6922ad92944c9e0120e1b3abd443564e9fe
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-08-22 22:01:56 +0530

    Day28_RegistrationValidationAssignment2

---
+++ Day28_Assignment2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Day28_Assignment2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
